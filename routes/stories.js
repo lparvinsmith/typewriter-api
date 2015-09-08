c8a19ac2822f7c0a7d0771fc2eeb02331c72ab10
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-09-08 11:15:13 -0400

    adds route for show one story

--- routes/stories.js
+++ routes/stories.js
@@ -18,4 +18,36 @@ router.route('/')
     });
   });
 
+router.route('/:id')
+  .all(function(req,res,next){
+    //add story id as property of response: makes res.locals.story available to each of following handlers
+    models.Story.findById(req.params.id).then(function(story){
+      res.locals.story = story;
+      console.log('req.params.id is:' +req.params.id + 'and story is: ' + res.locals.story);
+      next();
+    }, function(err){
+      next(err); //goes to fail handler in app, unless we set fail handler below with all
+    });
+  })
+  .get(function(req,res){
+    res.json(res.locals.story);
+  })
+  // .patch(function(req,res){
+  //   // updates db using req.body
+  //   res.locals.story.update(req.body).then(function(story){
+  //     res.json(story);
+  //   }, function(err){
+  //     res.sendStatus(500);
+  //   });
+  // })
+  // .delete(function(req,res){
+  //   res.send("story#delete");
+  // })
+  // fail handler -- next(err) leads here
+  .all(function(err, req, res, next){
+    if (err){
+      res.sendStatus(404);
+    }
+  });
+
 module.exports = router;
