ecafd4a2eed2aed1cbcc9ba1060a0a9f3fb25485
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-09-08 09:43:12 -0400

    adds authentication routes: signup, login, logout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var async = require('async');
+var bcrypt = require('bcrypt');
+var models = require('../models/index'),
+    User = models.User;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+////// AUTHENTICATION ///////
+
+// REGISTER
+router.route('/signup')
+  .get(function(req, res, next) {
+    res.sendStatus(405);
+  })
+  .post(function(req, res, next) {
+    if(!req.body || !req.body.email || !req.body.password) {
+      var err = new Error("No credentials.");
+      return next(err);
+    }
+
+    async.waterfall([
+      function(cb) {
+        bcrypt.genSalt(16, cb);
+      },
+      function(salt, cb) {
+        bcrypt.hash(req.body.password, salt, cb);
+      },
+      function(hash, cb) {
+        User.create({
+          email : req.body.email,
+          password : hash,
+          name : req.body.name
+        }).then(function(user) {
+          cb(null, user);
+        }, cb);
+      }
+      // should also log in user here
+    ], function(err, result) {
+      if(err) {
+        return next(err);
+      }
+
+      res.sendStatus(201);
+    });
+  });
+
+// LOG IN
+router.route('/login')
+  .get(function(req, res, next) {
+    res.sendStatus(405);
+  })
+  .all(function(req,res, next){
+    console.log(req.body.email);
+    console.log(req.body.password);
+    next();
+  })
+  .post(passport.authenticate('local'), function(req, res){
+    res.sendStatus(201);
+  });
+
+// LOG OUT
+router.route('/logout')
+  .all(function(req, res, next) {
+    // if (!req.user) {
+    //   var err = new Error("Log in first.");
+    //   return next(err);
+    // }
+    req.logout();
+    res.sendStatus(200);
+  });
 
 module.exports = router;
