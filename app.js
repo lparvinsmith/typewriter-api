63518cb9405735314aaf877c49a6d64549075e46
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-09-07 21:18:36 -0400

    deletes generated routes/users - will put auth in routes/index.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var stories = require('./routes/stories');
+// var sections = require('./routes/sections');
+// var images = require('./routes/images');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/stories', stories);
+// app.use('/sections', sections);
+// app.use('/images', images);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
